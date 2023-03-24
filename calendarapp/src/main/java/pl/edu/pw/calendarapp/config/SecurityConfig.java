fa1c161f5d1acdee56e5c6f8149c45eabc1aacd0
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2023-03-24 20:59:44 +0100

    add basic security and cors

---
+++ calendarapp/src/main/java/pl/edu/pw/calendarapp/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package pl.edu.pw.calendarapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+                .httpBasic();
+        return http.build();
+    }
+}
