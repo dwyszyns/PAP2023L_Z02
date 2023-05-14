d955e606f585462d81cfba05c4b03c3ce51e6daf
Rafał Cendrowski <rafal.cendrowski@protonmail.com>
2023-05-14 15:50:15 +0200

    add methods for adding and subscribing members to a calendar

--- calendarapp/src/main/java/pl/edu/pw/calendarapp/member/rest/MemberController.java
+++ calendarapp/src/main/java/pl/edu/pw/calendarapp/member/rest/MemberController.java
@@ -1,8 +1,11 @@
 package pl.edu.pw.calendarapp.member.rest;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import pl.edu.pw.calendarapp.auth.bizz.AuthUtil;
 import pl.edu.pw.calendarapp.member.bizz.MemberMapper;
 import pl.edu.pw.calendarapp.member.bizz.MemberService;
 import pl.edu.pw.calendarapp.member.repo.MemberUserProjection;
@@ -20,13 +23,14 @@ public class MemberController {
     public MemberView getCurrentMember() {
         final MemberUserProjection memberFromAuth =
                 (MemberUserProjection) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        return MemberMapper.mapMember(memberService.findById(memberFromAuth.getMemberId()));
+        return MemberMapper.mapMember(memberService.findById(memberFromAuth.getMemberId()).orElse(null));
 
     }
 
     @GetMapping("/{memberId}")
     public MemberView getMemberById(@PathVariable("memberId") final long memberId) {
-        return MemberMapper.mapMember(memberService.findById(memberId));
+        return MemberMapper.mapMember(memberService.findById(memberId).orElseThrow(() ->
+                new ResponseStatusException(HttpStatus.NOT_FOUND, "Member not found")));
     }
 
     @GetMapping("/{memberId}/friends")
@@ -39,19 +43,28 @@ public class MemberController {
     @PostMapping("/{memberId}/friends/{requestId}")
     public void acceptFriendRequest(@PathVariable("memberId") final long memberId,
                                     @PathVariable("requestId") final long requestId) {
+        authorizeMember(memberId);
         memberService.acceptFriendRequest(requestId, memberId);
     }
 
     @DeleteMapping("/{memberId}/friends/{requestId}")
     public void rejectFriendRequest(@PathVariable("memberId") final long memberId,
                                     @PathVariable("requestId") final long requestId) {
+        authorizeMember(memberId);
         memberService.rejectFriendRequest(requestId, memberId);
     }
 
     @PostMapping("/{memberId}/friends")
     public void sendFriendRequest(@PathVariable("memberId") final long memberId,
                                   @RequestParam("friendId") final long friendId) {
+        authorizeMember(memberId);
         memberService.sendFriendRequest(memberId, friendId);
+    }
+
+    private void authorizeMember(final long memberId) {
+        if (AuthUtil.getMemberIdFromSecurityContext() != memberId) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+        }
     }
 
 }
